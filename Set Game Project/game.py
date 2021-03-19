cef41916e5ecea3cb3f5c226288cce9e145e4135
apawate <apawate739@student.fuhsd.org>
2021-03-19 14:42:14 -0700

    Fixed leaderboard formatting issues.

--- Set Game Project/game.py
+++ Set Game Project/game.py
@@ -285,7 +285,7 @@ def playRealtimeRound(deck, upCards, players): # playRound function, the main fu
       
       elif description == "leaderboard":
           for x in range(int(str(urlopen("https://setgame.lentil1023.repl.co/numofplayers").read())[2:3])):
-              print("Name: ", str(urlopen("https://setgame.lentil1023.repl.co/getname").read()[1:]), "Score: ", str(urlopen("https://setgame.lentil1023.repl.co/getscore").read()[1:]))
+              print("Name: ", str(urlopen("https://setgame.lentil1023.repl.co/getname").read())[1:], "Score: ", str(urlopen("https://setgame.lentil1023.repl.co/getscore").read())[1:])
 
 
       elif description == "q": # If the user wants to quit:
